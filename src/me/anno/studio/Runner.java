048487dfd7c30a9e5086a275f47f93a32c5a24b8
Antonio Noack <antonio-noack@gmx.de>
2021-05-15 09:44:34 +0200

    joined CLI into the base build; added that to 1.1.3

--- src/me/anno/studio/Runner.java
+++ src/me/anno/studio/Runner.java
@@ -9,10 +9,36 @@ package me.anno.studio;
 // done particle systems
 // done move entries in tree
 
+import me.anno.studio.cli.RemsCLI;
 import me.anno.studio.rems.RemsStudio;
 
 public class Runner {
-    public static void main(String[] args){
-        RemsStudio.main(args);
+
+    private static boolean contains(String[] values, String query) {
+        if (values.length == 0) return false;
+        String query2 = "-" + query;
+        String query3 = "--" + query;
+        String query4 = query + "=";
+        for (String value : values) {
+            if (value == null) continue;
+            String lcValue = value.toLowerCase();
+            if (lcValue.equals(query) || lcValue.equals(query2) || lcValue.equals(query3) || lcValue.startsWith(query4)) {
+                return true;
+            }
+        }
+        return false;
     }
+
+    public static void main(String[] args) {
+        if (contains(args, "o") || contains(args, "output") ||
+                contains(args, "?") || contains(args, "help") ||
+                contains(args, "i") || contains(args, "input")) {
+            // somebody wants to use it as a console
+            RemsCLI.main(args);
+        } else {
+            // start the editor
+            RemsStudio.main(args);
+        }
+    }
+
 }
