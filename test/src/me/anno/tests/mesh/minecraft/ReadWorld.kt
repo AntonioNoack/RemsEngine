3118493cbf00ab231248334d85500581f53a40b9
Antonio Noack <antonio-noack@gmx.de>
2023-12-12 20:49:23 +0100

    Simplified block->color logic in Minecraft reader experiment

--- test/src/me/anno/tests/mesh/minecraft/ReadWorld.kt
+++ test/src/me/anno/tests/mesh/minecraft/ReadWorld.kt
@@ -25,106 +25,148 @@ class MonoModel(val color: Int) : VoxelModel(16, 16, 16) {
     override fun getBlock(x: Int, y: Int, z: Int): Int = color
 }
 
+fun stripEnd(name: String, end: String): String {
+    return if (name.endsWith(end)) name.substring(0, name.length - end.length)
+    else name
+}
+
+fun stripStart(name: String, start: String): String {
+    return if (name.startsWith(start)) name.substring(start.length)
+    else name
+}
+
+private val colorMap = HashMap<String?, Int>(256)
 fun blockToColor(block: Map<String, Any>): Int {
-    var name = block["Name"].toString()
-    if (name == "minecraft:air") return 0
-    if (name.startsWith("minecraft:")) name = name.substring("minecraft:".length)
+    val name = block["Name"].toString()
+    return colorMap.getOrPut(name) { blockToColor0(name) }
+}
+
+private val starts = listOf(
+    "minecraft:", "infested_", "cracked_", "polished_", "chiseled_", "potted_"
+)
+private val ends = listOf(
+    "_stained_glass_pane", "_stained_glass", "_carpet", "_concrete_powder", "_concrete",
+    "_slab", "_stairs", "_wool", "_wall", "_door", "_trapdoor", "_glazed_terracotta", "_terracotta",
+    "_shulker_box", "_bed", "_fence_gate", "_fence", "_planks", "_pressure_plate", "_tulip", "_block"
+)
+
+fun blockToColor0(name0: String): Int {
+    var name = name0
+    for (start in starts) {
+        name = stripStart(name, start)
+    }
+    for (end in ends) {
+        name = stripEnd(name, end)
+    }
     return when (name) {
-        "stone", "stone_brick_slab", "stone_bricks", "brick_slab" -> 0x777777
-        "smooth_quartz", "smooth_stone_slab" -> 0x888888
-        "quartz_block", "quartz_slab", "quartz_stairs", "quartz_pillar",
-        "chiseled_quartz_block" -> 0xccc7bb
-        "dirt" -> 0x75432f
+        "stone", "stone_brick", "stone_bricks", "bricks", "terracotta" -> 0x777777
+        "smooth_quartz", "smooth_stone" -> 0x888888
+        "quartz", "quartz_pillar" -> 0xccc7bb
+        "dirt", "farmland" -> 0x75432f
         "coarse_dirt" -> 0x9e6047
         "gravel" -> 0x555555
-        "anvil" -> 0x444444
+        "wheat" -> 0xb3a268
+        "anvil", "chipped_anvil" -> 0x444444
         "iron_ore" -> 0x776655
         "coal_ore" -> 0x222222
         "gold_ore" -> 0x887766
         "redstone_ore" -> 0xff6666
         "lapis_ore" -> 0x5577ff
         "diamond_ore" -> 0x77ffff
+        "diamond" -> 0x22ffff
         "bedrock" -> 0x000000
-        "water", "seagrass", "tall_seagrass", "kelp" -> 0xaaccff
+        "water", "seagrass", "tall_seagrass", "kelp_plant" -> 0xaaccff
+        "kelp" -> 0x2b1c17 // right?
         "lava" -> 0xffaa33
         "snow" -> 0xffffff
-        "obsidian" -> 0x201122
-        "cut_sandstone", "chiseled_sandstone", "sandstone_stairs", "sandstone_slab" -> 0xd9d0b6
-        "chiseled_red_sandstone", "red_sandstone", "red_sandstone_stairs", "smooth_red_sandstone",
-        "cut_red_sandstone", "red_sandstone_slab", "red_sandstone_wall" -> 0xbd7e7e
-        "nether_brick_slab", "nether_brick_fence", "nether_brick_stairs",
-        "nether_bricks", "red_nether_brick_slab" -> 0x730e05
+        "deepslate", "deepslate_tile", "cobbled_deepslate", "deepslate_tiles" -> 0x333333
+        "obsidian", "crying_obsidian" -> 0x201122
+        "cut_sandstone", "sandstone", "sand" -> 0xd9d0b6
+        "red_sandstone", "smooth_red_sandstone", "cut_red_sandstone", "red_sand" -> 0xbd7e7e
+        "nether_brick", "nether_bricks", "red_nether_brick", "red_nether_bricks", "red_mushroom", "nether_wart" -> 0x730e05
         "netherrack" -> 0xa83328
-        "wall_torch", "glowstone" -> 0xffff77
-        "andesite", "polished_andesite" -> 0x888888
-        "cobblestone", "cobblestone_stairs", "cobblestone_slab", "cobblestone_wall" -> 0x666666
-        "red_carpet", "red_wool", "red_concrete", "red_concrete_powder",
-        "red_terracotta" -> 0xff3333
-        "white_concrete", "white_wool", "white_terracotta",
-        "white_carpet", "white_stained_glass", "white_stained_glass_pane" -> 0xdddddd
-        "cyan_concrete", "cyan_wool", "cyan_terracotta",
-        "cyan_stained_glass" -> 0x33ffff
-        "purple_wool", "purple_terracotta", "purple_stained_glass" -> 0xff33ff
-        "green_concrete", "green_wool", "green_carpet" -> 0x33ff33
-        "black_wool", "black_terracotta", "black_stained_glass_pane",
-        "black_carpet" -> 0x333333
-        "orange_wool", "orange_terracotta", "orange_carpet",
-        "orange_stained_glass", "orange_concrete" -> 0xffaa33
-        "lime_wool", "lime_terracotta", "lime_carpet" -> 0xc4ff77
-        "brown_wool", "brown_stained_glass_pane", "brown_carpet" -> 0x5e351d
-        "magenta_wool", "magenta_stained_glass", "magenta_shulker_box",
-        "magenta_terracotta" -> 0xd424ce
-        "blue_wool", "blue_carpet", "blue_stained_glass_pane" -> 0x3333ff
-        "pink_wool", "pink_carpet", "pink_stained_glass_pane" -> 0xeb6ee6
-        "yellow_concrete", "yellow_wool", "yellow_terracotta",
-        "wet_sponge", "yellow_carpet" -> 0xffff33
-        "light_gray_concrete", "light_gray_concrete_powder",
-        "light_gray_wool", "light_gray_stained_glass_pane",
-        "light_gray_stained_glass", "light_gray_carpet" -> 0xcccccc
-        "gray_concrete", "gray_concrete_powder", "gray_wool",
-        "gray_terracotta", "gray_stained_glass", "gray_carpet",
-        "gray_stained_glass_pane" -> 0x777777
-        "light_blue_concrete", "light_blue_concrete_powder", "light_blue_wool",
-        "light_blue_terracotta", "light_blue_stained_glass_pane" -> 0xaabbee
+        "wall_torch", "glowstone", "lantern", "torch" -> 0xffff77
+        "soul_wall_torch" -> 0x9999ff
+        "andesite" -> 0x888888
+        "cobblestone" -> 0x666666
+        "red", "poppy" -> 0xff3333
+        "brick" -> 0xc9483e
+        "beehive" -> 0xc9902c
+        "white", "skeleton_skull", "skeleton_wall_skull", "cake", "bone", "lily_of_the_valley" -> 0xdddddd
+        "cyan" -> 0x33ffff
+        "purple" -> 0xff33ff
+        "green", "melon", "zombie_head" -> 0x33ff33
+        "black", "black_glazed", "dragon_egg", "wither_skeleton_skull", "blackstone", "spawner",
+        "gilded_blackstone" -> 0x333333
+        "orange", "pumpkin" -> 0xffaa33
+        "granite" -> 0xc77e65
+        "end_stone_bricks", "end_stone", "end_rod", "end_portal" -> 0xd6c096
+        "lapis" -> 0x3377ff
+        "slime", "emerald", "creeper_head" -> 0x77ff77
+        "dropper", "dispenser", "hopper", "cauldron" -> 0x444444
+        "lime" -> 0xc4ff77
+        "brown" -> 0x5e351d
+        "magenta" -> 0xd424ce
+        "blue", "blue_orchid" -> 0x3333ff
+        "pink" -> 0xeb6ee6
+        "yellow", "sponge", "wet_sponge" -> 0xffff33
+        "light_gray" -> 0xcccccc
+        "gray" -> 0x777777
+        "light_blue" -> 0xaabbee
         "oak_leaves", "azalea_leaves" -> 0x8cf591
         "jungle_leaves" -> 0x49d150
         "birch_leaves", "acacia_leaves" -> 0x95ed9a
         "dark_oak_leaves" -> 0x418045
         "spruce_leaves" -> 0x4e7351
         "rail", "iron_bars" -> 0x555555
-        "redstone_block", "tnt" -> 0xff2222
-        "iron_block", "iron_door", "iron_trapdoor" -> 0xaaaaaa
-        "raw_iron_block" -> 0x776655
-        "jungle_stairs", "jungle_fence", "jungle_trapdoor", "jungle_planks", "jungle_slab" -> 0x7d4b09
-        "coal_block" -> 0x111111
-        "grass_block", "tall_grass", "large_fern" -> 0x77ff77
-        "oxeye_daisy", "azure_bluet" -> 0xe6d8b1
+        "detector_rail" -> 0x665555
+        "powered_rail" -> 0x666055
+        "redstone", "tnt", "redstone_wall_torch" -> 0xff2222
+        "heavy_weighted", "gold" -> 0xffc900
+        "iron", "light_weighted" -> 0xaaaaaa
+        "raw_iron" -> 0x776655
+        "player_head", "player_wall_head" -> 0x777777 // could be any color
+        "jungle" -> 0x7d4b09
+        "coal" -> 0x111111
+        "diorite" -> 0xbbbbbb
+        "grass", "tall_grass", "large_fern", "rose_bush",  "moss" -> 0x77ff77
+        "oxeye_daisy", "azure_bluet", "peony" -> 0xe6d8b1
         "mycelium" -> 0xbd61c7
-        "mossy_cobblestone_wall", "mossy_stone_bricks", "mossy_cobblestone" -> 0x364a31
-        "oak_fence", "oak_trapdoor", "oak_planks", "oak_door", "oak_fence_gate", "oak_slab" -> 0xbd7659
-        "birch_fence", "birch_fence_gate", "birch_door", "birch_trapdoor", "birch_slab",
-        "birch_stairs" -> 0xb8aa86
+        "mossy_stone_bricks", "mossy_cobblestone" -> 0x364a31
+        "furnace" -> 0x555555
+        "birch" -> 0xb8aa86
         "birch_log" -> 0xdeccb4
         "acacia_log" -> 0x524b42
-        "acacia_trapdoor", "acacia_stairs", "acacia_door", "acacia_slab" -> 0xa36615
+        "acacia" -> 0xa36615
         "oak_log" -> 0x523723
+        "oak", "stripped_oak_wood", "stripped_oak_log", "lectern", "scaffolding", "bookshelf", "campfire", "petrified_oak",
+        "piston", "sticky_piston", "chest", "trapped_chest", "piston_head" -> 0xbd7659
         "stripped_dark_oak_log", "dark_oak_log" -> 0x2e231a
-        "dark_oak_slab", "dark_oak_door", "dark_oak_trapdoor", "dark_oak_stairs",
-        "stripped_dark_oak_wood", "dark_oak_fence" -> 0x664325
-        "stripped_jungle_log" -> 0x634832
-        "stripped_spruce_log", "spruce_slab" -> 0x4f3b2b
+        "dark_oak", "stripped_dark_oak_wood" -> 0x664325
+        "stripped_jungle_log", "jungle_wood" -> 0x634832
+        "stripped_spruce_log", "spruce", "stripped_spruce_wood" -> 0x4f3b2b
         "spruce_log" -> 0x261b12
         "jungle_log" -> 0x333118
         "mushroom_stem" -> 0xeeeeee
         "basalt" -> 0x38354d
         "fire" -> 0xfa901e
-        "redstone_wire", "glass", "fern", "grass", "repeater", "magenta_candle", "candle", "pink_candle",
-        "cyan_candle", "orange_candle", "orange_wall_banner", "birch_wall_sign", "light_blue_wall_banner",
-        "tripwire", "dead_brain_coral_fan", "oak_wall_sign", "birch_sign", "acacia_button",
-        "acacia_wall_sign", "ladder", "vine", "dark_oak_button", "dark_oak_wall_sign" -> return 0 // air
-        else -> {
-            LOGGER.warn("Unknown block $block")
-            0xff00ff
+        "allium" -> 0xc164d1
+        "clay" -> 0xafb9ba
+        "purpur",  "shulker_box" -> 0xd096d6
+        "repeating_command", "chain_command", "command", "jigsaw" -> 0x998866
+        "redstone_wire", "glass", "glass_pane", "fern",  "repeater", "candle", "tripwire",
+        "ladder", "vine", "lever", "cave_vines", "cave_vines_plant",
+        "tripwire_hook", "barrier", "comparator", "air", "flower_pot" -> return 0 // air
+        else -> when {
+            name.endsWith("_banner") || name.endsWith("_candle") || name.endsWith("_button")
+                    || name.endsWith("_sign") || name.endsWith("_coral_fan") ->
+                return 0 // air
+            name.endsWith("_coral") -> 0x777777
+            name.endsWith("_sapling") -> 0xcc5555
+            else -> {
+                LOGGER.warn("Unknown block $name")
+                0xff00ff
+            }
         }
     } or black
 }
@@ -134,7 +176,7 @@ fun main() {
     // load a section of a Minecraft world
     // todo separate transparent/non-transparent, and make glass transparent
     val world = Entity()
-    world.add(StaticMeshManager()) // hack
+    world.add(StaticMeshManager()) // performance hack
     val file = downloads.getChild("r.0.0.mca")
     val region = RegionFormatReader.read(file.readByteBufferSync(false))
     val chunkData = Array(1024) {
@@ -222,6 +264,7 @@ fun main() {
                 } else null
             }
             val mesh = model.createMesh(null, null, { xi, yi, zi ->
+                // todo there is some cracks here or there
                 val side = when {
                     yi < 0 && xi in 0 until 16 && zi in 0 until 16 -> BlockSide.NY
                     yi >= 16 && xi in 0 until 16 && zi in 0 until 16 -> BlockSide.PY
