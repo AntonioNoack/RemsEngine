3020c66d84d1751b6fc8735e30c646f5f5cdb6c4
Antonio Noack <antonio-noack@gmx.de>
2024-03-17 17:08:34 +0100

    Removed unused audio loaders (2)

--- Video/src/me/anno/video/ffmpeg/FFMPEGAudio.kt
+++ Video/src/me/anno/video/ffmpeg/FFMPEGAudio.kt
@@ -5,10 +5,10 @@ import me.anno.audio.openal.SoundBuffer
 import me.anno.cache.AsyncCacheData
 import me.anno.cache.IgnoredException
 import me.anno.io.BufferedIO.useBuffered
-import me.anno.io.Streams.readLE16
 import me.anno.io.files.FileReference
 import me.anno.utils.ShutdownException
 import me.anno.utils.pooling.ByteBufferPool
+import me.anno.utils.types.Buffers.flip16
 import me.anno.utils.types.InputStreams.readNBytes2
 import org.apache.logging.log4j.LogManager
 import org.lwjgl.openal.AL11.AL_FORMAT_MONO16
@@ -25,6 +25,10 @@ class FFMPEGAudio(file: FileReference?, val channels: Int, val sampleRate: Int, 
 
     companion object {
         private val LOGGER = LogManager.getLogger(FFMPEGAudio::class)
+    }
+
+    init {
+        if (channels !in 1..2) throw IllegalStateException("Unsupported number of audio channels: $channels")
     }
 
     override fun process(process: Process, vararg arguments: String) {
@@ -45,23 +49,22 @@ class FFMPEGAudio(file: FileReference?, val channels: Int, val sampleRate: Int, 
         }
         process.inputStream.useBuffered().use { input: InputStream ->
             val frameCount = (sampleRate * duration).toInt()
-            input.mark(1)
-            if (input.read() >= 0) {
-                input.reset()
-                val buffer = SoundBuffer()
-                try {
-                    val (bytes, shorts, stereo) = readRAW(input, channels, frameCount)
+            val buffer = SoundBuffer()
+            result.value = try {
+                val (bytes, shorts, stereo) = readRAW(input, channels, frameCount)
+                if (bytes.limit() == 0) { // EOF
+                    ByteBufferPool.free(bytes)
+                    LOGGER.warn("No data was available for $file")
+                    null
+                } else {
                     buffer.loadRaw16(
                         shorts, bytes, sampleRate,
                         if (stereo) AL_FORMAT_STEREO16 else AL_FORMAT_MONO16
                     )
-                    result.value = buffer
-                } catch (_: IgnoredException) {
-                    result.value = null
+                    buffer
                 }
-            } else {// EOF
-                LOGGER.warn("No data was available for $file")
-                result.value = null
+            } catch (_: IgnoredException) {
+                null
             }
         }
     }
@@ -69,46 +72,16 @@ class FFMPEGAudio(file: FileReference?, val channels: Int, val sampleRate: Int, 
     val result = AsyncCacheData<SoundBuffer>()
 
     fun readRAW(input: InputStream, channels: Int, frameCount: Int): Triple<ByteBuffer, ShortBuffer, Boolean> {
-
         val sampleCount = frameCount * channels
         val size = sampleCount * 2
         val bytes = ByteBufferPool.allocateDirect(size)
+        input.readNBytes2(size, bytes, false)
+        if (bytes.position() > 0) bytes.flip()
         val shorts = bytes.asShortBuffer()
-        if (channels !in 1..2) throw RuntimeException("Unsupported number of audio channels: $channels")
-
-        when (channels) {
-            1 -> {
-
-                if (shorts.order() == ByteOrder.LITTLE_ENDIAN) {// fast path
-                    input.readNBytes2(size, bytes, false)
-                } else {// if our program is ever executed on a big endian machine
-                    for (i in 0 until sampleCount) {
-                        shorts.put(input.readLE16().toShort())
-                    }
-                }
-
-                if (bytes.position() > 0) bytes.flip()
-                if (shorts.position() > 0) shorts.flip()
-
-                return Triple(bytes, shorts, false)
-            }
-            2 -> {
-
-                if (shorts.order() == ByteOrder.LITTLE_ENDIAN) {// fast path
-                    input.readNBytes2(size, bytes, false)
-                } else {// if our program is ever executed on a big endian machine
-                    for (i in 0 until sampleCount) {
-                        shorts.put(input.readLE16().toShort())
-                    }
-                }
-
-                if (bytes.position() > 0) bytes.flip()
-                if (shorts.position() > 0) shorts.flip()
-
-                return Triple(bytes, shorts, true)
-            }
-            else -> throw IllegalStateException()
+        if (shorts.order() == ByteOrder.BIG_ENDIAN) {
+            bytes.flip16()
         }
+        return Triple(bytes, shorts, channels == 2)
     }
 
     override fun destroy() {}
--- src/me/anno/utils/types/Buffers.kt
+++ src/me/anno/utils/types/Buffers.kt
@@ -1,10 +1,20 @@
 package me.anno.utils.types
 
 import java.nio.Buffer
+import java.nio.ByteBuffer
 
 object Buffers {
     @JvmStatic
     fun Buffer.skip(n: Int) {
         position(position() + n)
+    }
+
+    @JvmStatic
+    fun ByteBuffer.flip16() {
+        for (i in position() until limit() - 1 step 2) {
+            val t = this[i]
+            put(i, this[i + 1])
+            put(i + 1, t)
+        }
     }
 }
