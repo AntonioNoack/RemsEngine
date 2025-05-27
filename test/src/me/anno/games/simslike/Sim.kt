2a12999469c1e988856e3fc38c9a9d72ccb64c17
Antonio Noack <antonio-noack@gmx.de>
2025-05-27 17:32:51 +0200

    Small cleanup

--- test/src/me/anno/games/simslike/Sim.kt
+++ test/src/me/anno/games/simslike/Sim.kt
@@ -3,13 +3,8 @@ package me.anno.games.simslike
 import me.anno.ecs.Component
 import me.anno.ecs.EntityQuery.getComponent
 import me.anno.ecs.components.anim.AnimMeshComponent
-import me.anno.ecs.components.anim.Animation
-import me.anno.ecs.components.anim.AnimationCache
 import me.anno.ecs.systems.OnUpdate
 import me.anno.maths.Maths.clamp
-import me.anno.maths.Maths.posMod
-import org.joml.Matrix4x3f
-import org.joml.Vector3f
 import kotlin.math.abs
 import kotlin.math.max
 
@@ -30,22 +25,10 @@ class Sim : Component(), OnUpdate {
         val transform = transform ?: return
         val nav0 = getComponent(SimNavAgent::class) ?: return
         val nav1 = nav0.crowdAgent ?: return
-        val pos = nav1.currentPosition
-        transform.setLocalPosition(
-            pos.x - offset.x,
-            pos.y - offset.y,
-            pos.z - offset.z
-        )
+        transform.setLocalPosition(nav1.currentPosition)
         invalidateAABB()
         // todo update rotation
     }
-
-    // todo remove root offset from animation
-    //   offset: mix(offset0, offset1, relativeAnimationTime)
-
-    // todo or alternatively, we could use these animations properly, and teleport on each restart...
-
-    private val offset = Vector3f()
 
     fun updateAnimationState() {
         val anim = getComponent(AnimMeshComponent::class) ?: return
@@ -56,27 +39,6 @@ class Sim : Component(), OnUpdate {
         anim.animations[0].weight = weight(speed)
         anim.animations[1].weight = weight(speed - 1f)
         anim.animations[2].weight = weight(speed - 2f)
-        calculateOffset(anim)
-    }
-
-    private fun calculateOffset(anim: AnimMeshComponent): Vector3f {
-        offset.set(0f)
-        for (state in anim.animations) {
-            val animation = AnimationCache[state.source] ?: continue
-            val t = posMod(state.progress / animation.duration, 1f)
-            val offsetI = calculateOffset(animation, t)
-            offsetI.mulAdd(state.weight, offset, offset)
-        }
-        return offset
-    }
-
-    private fun calculateOffset(anim: Animation, t: Float): Vector3f {
-        val offset0 = Vector3f()
-        val offset1 = Vector3f()
-        val tmpMatrix = Matrix4x3f()
-        anim.getMatrix(0, 0, listOf(tmpMatrix))?.getTranslation(offset0)
-        anim.getMatrix(anim.numFrames - 1, 0, listOf(tmpMatrix))?.getTranslation(offset1)
-        return offset0.mix(offset1, t)
     }
 
     companion object {
