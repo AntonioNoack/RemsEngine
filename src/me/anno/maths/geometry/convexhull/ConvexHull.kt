041b853fbffa4afc360a3a027c0593ea9607fe30
Antonio Noack <antonio-noack@gmx.de>
2026-09-17 15:36:34 +0200

    Added ConvexHull.toMesh()

--- src/me/anno/maths/geometry/convexhull/ConvexHull.kt
+++ src/me/anno/maths/geometry/convexhull/ConvexHull.kt
@@ -1,7 +1,9 @@
 package me.anno.maths.geometry.convexhull
 
+import me.anno.ecs.components.mesh.Mesh
 import me.anno.utils.algorithms.ForLoop.forLoopSafely
 import me.anno.utils.pooling.JomlPools
+import me.anno.utils.types.Arrays.resize
 import me.anno.utils.types.Triangles.subCross
 import org.joml.Vector3d
 import kotlin.math.max
@@ -50,5 +52,21 @@ class ConvexHull(val vertices: ArrayList<Vector3d>, val triangles: IntArray) {
         }
         JomlPools.vec3d.sub(2)
         return true
+    }
+
+    fun toMesh(mesh: Mesh = Mesh()): Mesh {
+        val vertices = vertices
+        val positions = mesh.positions.resize(vertices.size * 3)
+        for (i in vertices.indices) {
+            val k = i * 3
+            val v = vertices[i]
+            positions[k] = v.x.toFloat()
+            positions[k + 1] = v.y.toFloat()
+            positions[k + 2] = v.z.toFloat()
+        }
+        mesh.positions = positions
+        mesh.indices = triangles
+        mesh.invalidateGeometry()
+        return mesh
     }
 }
